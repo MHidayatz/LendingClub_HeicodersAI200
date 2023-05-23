9c55496f9e146c6c19df4518ca65db952964f3d3
MHidayatz <mhidayatz86@gmail.com>
2023-05-23 20:26:22 +0800

    Update requirements

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ html_temp = """
 def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-local_css("Style/style.css")
+local_css("style/style.css")
 
 def main():
     st.title("Heicoders AI200: Applied Machine Learning")
